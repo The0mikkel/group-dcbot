8537f3b01d2aaf2aa2cbf3374b466fb4458cf073
28625667 <mikkel.alb@gmail.com>
2023-01-24 00:35:34 +0100

    Updated help command to slash system

--- src/commands/utility/Help.ts
+++ src/commands/utility/Help.ts
@@ -1,8 +1,8 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Message } from "discord.js";
+import { ActionRowBuilder, AutocompleteInteraction, ButtonBuilder, ButtonStyle, CacheType, ChatInputCommandInteraction, EmbedBuilder, Message, SlashCommandBuilder } from "discord.js";
 import BotSystem from "../../data/BotSystem";
 import Command from "../../data/Command/Command";
 import Commands from "../../data/Command/Commands";
-import CommandType from "../../data/Command/Types/CommandType";
+import CommandType from "../../data/Command/Interfaces/CommandType";
 import UtilityCommand from "../../data/Command/Types/UtilityCommand";
 import { UserLevel } from "../../data/Command/UserLevel";
 import Translate from "../../data/Language/Translate";
@@ -29,7 +29,50 @@ export default class help extends UtilityCommand {
 		this.image = BotSystem.client?.user?.avatarURL() ?? "";
 	}
 
-	async execute(message: Message, botSystem: BotSystem, args: any): Promise<void> {
+	slashCommand(): SlashCommandBuilder 
+	{
+		let command = super.slashCommand();
+
+		command.setNameLocalizations({
+			"en-US": "help",
+			"da": "hjælp"
+		});
+
+		command.setDescriptionLocalizations({
+			"en-US": "Get help on how to use the bot",
+			"da": "Få hjælp til hvordan du bruger boten"
+		});
+
+		command.addStringOption(option =>
+			option.setName('command')
+				.setNameLocalizations({
+					"en-US": "command",
+					"da": "kommando"
+				})
+				.setDescription("The command you want help with")
+				.setDescriptionLocalizations({
+					"en-US": "The command you want help with",
+					"da": "Kommandoen du vil have hjælp med"
+				})
+				.setRequired(false)
+				.setMinLength(1)
+				.setAutocomplete(true)
+		);
+		
+		return command;
+	}
+
+	async executeAutocomplete(interaction: AutocompleteInteraction<CacheType>, botSystem: BotSystem): Promise<void> {
+		const allCommands = Commands.commands;
+
+		let commandNames: string[] = allCommands.map(command => command.name);
+		let commandAliases: string[] = [] // allCommands.map(command => command.aliases).flat();
+		let commandNamesAndAliases = commandNames.concat(commandAliases);
+
+		this.autocompleteHelper(interaction, commandNamesAndAliases);
+	}
+
+	async execute(interaction: ChatInputCommandInteraction, botSystem: BotSystem): Promise<void> {
 		this.translator = botSystem.translator;
 
 		if (this.image == "") {
@@ -38,43 +81,43 @@ export default class help extends UtilityCommand {
 
 		let commands = Commands.commands;
 
-		if (!args.length) { // General command
+		const commandSearched = interaction.options.getString('command');
+
+		if (!commandSearched) { // General command
 			let helpPage = new help();
-			helpPage.pageHelp(message, botSystem);
+			helpPage.pageHelp(interaction, botSystem);
 			return;
 		}
 
-		const name = args[0].toLowerCase();
+		const name = commandSearched.toLowerCase();
 
 		if (name == "all") {
-			this.commonHelp(message, botSystem);
+			this.commonHelp(interaction, botSystem);
 			return;
 		}
 
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) {
-			message.reply(this.translator.translate("that's not a valid command!"));
+			interaction.editReply(this.translator.translate("that's not a valid command!"));
 			return
 		}
 
-		this.commandSpecificHelp(message, botSystem, command);
+		this.commandSpecificHelp(interaction, botSystem, command);
 	}
 
 	typemap: Map<string, Command[]>;
 	pages: CommandType[];
 	image: string;
 
-	private async pageHelp(message: Message, botSystem: BotSystem, page: string = this.category) {
+	private async pageHelp(interaction: ChatInputCommandInteraction, botSystem: BotSystem, page: string = this.category) {
 
-		const pageContent = await this.generateHelpPage(page, message, botSystem);
+		const pageContent = await this.generateHelpPage(page, interaction, botSystem);
 		if (!pageContent) {
 			return;
 		}
 
-		BotSystem.autoDeleteMessageByUser(message, 0);
-
-		let helpMessage = await message.channel.send(pageContent);
+		let helpMessage = await interaction.editReply(pageContent);
 
 		const collector = helpMessage.createMessageComponentCollector({ time: 150000 });
 		collector.on('collect', async i => {
@@ -83,17 +126,16 @@ export default class help extends UtilityCommand {
 			}
 
 			if (i.customId.startsWith("help-message;")) {
-				const pageContent = await this.generateHelpPage(i.customId.split(";")[1], message, botSystem);
+				const pageContent = await this.generateHelpPage(i.customId.split(";")[1], interaction, botSystem);
 				if (!pageContent) {
 					return;
 				}
 				await i.update(pageContent);
 			}
 		});
-		collector.on('end', () => BotSystem.autoDeleteMessageByUser(helpMessage, 0));
 	}
 
-	async generateHelpPage(page: string = this.category, message: Message, botSystem: BotSystem): Promise<{ embeds: any[], components: any[] } | false> {
+	async generateHelpPage(page: string = this.category, interaction: ChatInputCommandInteraction, botSystem: BotSystem): Promise<{ embeds: any[], components: any[] } | false> {
 		if (!this.typemap.has(page)) {
 			return false;
 		}
@@ -101,7 +143,7 @@ export default class help extends UtilityCommand {
 		if (this.pages.length <= 0) {
 			let pages: CommandType[];
 			pages = [];
-			
+
 			for (const commandListArray of this.typemap) {
 				const commandList = commandListArray[1];
 				if (!commandList[0]) continue;
@@ -109,7 +151,7 @@ export default class help extends UtilityCommand {
 				let hasOne = false;
 				for (let index = 0; index < commandList.length; index++) {
 					let command = commandList[index];
-					const authorized = (await command.authorized(message, botSystem));
+					const authorized = (await command.authorized(interaction, botSystem));
 					if (authorized === true) {
 						hasOne = true;
 					}
@@ -134,17 +176,16 @@ export default class help extends UtilityCommand {
 
 		for (let index = 0; index < pageCommands.length; index++) {
 			let command = pageCommands[index];
-			if ((await command.authorized(message, botSystem)) == true) {
+			if ((await command.authorized(interaction, botSystem)) == true) {
 				pageText += `**${command.name}**\n${command.description}\n`;
 			}
 		}
 
 		const pageEmbed = new EmbedBuilder()
 			.setColor('#0099ff')
-			.setTitle(this.translator.translate("Command list")+':')
+			.setTitle(this.translator.translate("Command list") + ':')
 			.setDescription(pageText)
-			.addFields({ name: this.translator.translate('prefix')+':', value: (botSystem.guild)?.config.prefix ?? "gr!" })
-			.addFields({ name: this.translator.translate('detailed help')+':', value: this.translator.translate("write the command name, after the help command, to see more details about the command") });
+			.addFields({ name: this.translator.translate('detailed help') + ':', value: this.translator.translate("write the command name, after the help command, to see more details about the command") });
 
 		const buttons = new ActionRowBuilder<ButtonBuilder>();
 		for (let index = 0; index < this.pages.length; index++) {
@@ -164,37 +205,35 @@ export default class help extends UtilityCommand {
 		return { embeds: [pageEmbed], components: [buttons] };
 	}
 
-	private commonHelp(message: Message, botSystem: BotSystem) {
+	private commonHelp(interaction: ChatInputCommandInteraction, botSystem: BotSystem) {
 		let commands = Commands.commands;
 
 		const exampleEmbed = new EmbedBuilder()
 			.setColor('#0099ff')
-			.setTitle(this.translator.translate("Command list")+':')
+			.setTitle(this.translator.translate("Command list") + ':')
 			.setDescription(commands.map(command => command.name).join('\n'))
-			.addFields({ name: this.translator.translate('prefix')+':', value: (botSystem.guild)?.config.prefix ?? "gr!" })
-			.addFields({ name: this.translator.translate('detailed help')+':', value: this.translator.translate("write the command name, after the help command, to see more details about the command") });
+			.addFields({ name: this.translator.translate('detailed help') + ':', value: this.translator.translate("write the command name, after the help command, to see more details about the command") });
 
-		message.channel.send({ embeds: [exampleEmbed] });
+		interaction.editReply({ embeds: [exampleEmbed] });
 		return;
 	}
 
-	private commandSpecificHelp(message: Message, botSystem: BotSystem, command: Command) {
+	private commandSpecificHelp(interaction: ChatInputCommandInteraction, botSystem: BotSystem, command: Command) {
 		let data = [];
 		data.push(`**${(this.translator.translate("name"))}:** ${command.name}`);
 
 		let translatedAliases: string[] = [];
-		if (command.aliases.length > 0) data.push(`**${this.translator.translate("aliases")}:** ${command.aliases.join(', ')}`);
+		// if (command.aliases.length > 0) data.push(`**${this.translator.translate("aliases")}:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**${this.translator.translate("description")}:** ${command.description}`);
-		if (command.usage) data.push(`**${this.translator.translate("usage")}:** ${(botSystem.guild)?.config.prefix}${command.name} ${command.usage}`);
+		// if (command.usage) data.push(`**${this.translator.translate("usage")}:** /${command.name} ${command.usage}`);
 
 		if (command.cooldown > 0) data.push(`**${this.translator.translate("Cooldown")}:** ${command.cooldown || 3} ${botSystem.translator.translateUppercase("second(s)")}`);
 
 		const specificHelp = new EmbedBuilder()
 			.setColor('#0099ff')
-			.setTitle(this.translator.translate("Command list")+':')
+			.setTitle(this.translator.translate("Command list") + ':')
 			.setDescription(data.join('\n'))
-			.addFields({ name: this.translator.translate('prefix')+':', value: (botSystem.guild)?.config.prefix ?? "gr!" });
-			
-		message.channel.send({ embeds: [specificHelp] });
+
+		interaction.editReply({ embeds: [specificHelp] });
 	}
 };
